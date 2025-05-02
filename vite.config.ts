ec024a7714647fd0736b7ad5383ccf128344887a
Eduqrator <eduqrator@gmail.com>
2025-05-03 01:10:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  base: process.env.NODE_ENV === 'production' ? '/buil1.1/' : '/',
+  }
 });
